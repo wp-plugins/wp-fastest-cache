8c8ce525f00d5fa873b9b6bc07da8f7767cc0d6f
emrevona <emrevona@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-15 22:05:21 +0000

    translation
    git-svn-id: https://plugins.svn.wordpress.org/wp-fastest-cache/trunk@822712 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/info.js
@@ -0,0 +1,53 @@
+;(function($) {
+	$(".get-info").click(function(e){
+		var input = $(this).prev(".inputCon").find("input");
+		var options = {"h4": "", "h1" : ""};
+		if(input.attr("id") == "wpFastestCacheNewPost"){
+
+		}else if(input.attr("id") == "wpFastestCacheMinifyHtml"){
+			options.h4 = "Minify HTML";
+			options.h1 = "Compacting HTML code, including any inline JavaScript and CSS contained in it, can save many bytes of data and speed up downloading, parsing, and execution time.";
+		}else if(input.attr("id") == "wpFastestCacheMinifyCss"){
+			options.h4 = "Minify CSS";
+			options.h1 = "Compacting CSS code can save many bytes of data and speed up downloading, parsing, and execution time.";
+		}else if(input.attr("id") == "wpFastestCacheGzip"){
+			options.h4 = "Enable Gzip Compression";
+			options.h1 = "<label>Reduce the size of files sent from your server to increase the speed to which they are transferred to the browser.</label><br><br>" + 
+						"- <label>Reduce sizes of pages by up to 70%</label><br>" + 
+						"- <label>Increase page speed</label><br>" +
+						"- <label>Cost-benefit ratio: high</label><br>";
+		}
+
+		options.type = input.attr("id");
+		modifyHelpTip(options);
+	});
+	function modifyHelpTip(options){
+		var helpTip = $('<div id="rule-help-tip" style="display: block;"><div title="Close Window" class="close-window"> </div><h4></h4><h1 class="summary-rec"></h1><p></p></div>');
+		var windowHeight;
+		var windowWidth;
+
+		helpTip.find("div.close-window").click(function(){
+			helpTip.remove();
+		});
+
+		helpTip.attr("data-type", options.type);
+		helpTip.find("h4").text(options.h4);
+		helpTip.find("h1").html(options.h1);
+
+		var prevHelpTip = $('div[data-type="' + options.type + '"]');
+		if(prevHelpTip.length > 0){
+			prevHelpTip.remove();
+		}else{
+			if($('#rule-help-tip').length > 0){
+				$('#rule-help-tip').remove();
+			}
+			$("body").append(helpTip);
+
+			Wpfclang.init(Wpfclang.language);
+			
+			windowHeight = ($(window).height() - helpTip.height())/2;
+			windowWidth = ($(window).width() - helpTip.width())/2;
+			helpTip.css({"top": windowHeight, "left": windowWidth});
+		}
+	}
+})(jQuery);
