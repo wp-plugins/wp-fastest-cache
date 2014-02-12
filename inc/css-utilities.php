0d0fc1b915d98f75934ce6b9002aed1698edfee5
emrevona <emrevona@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-12 09:02:18 +0000

    Empty delimiter of strpos() has been solved
    git-svn-id: https://plugins.svn.wordpress.org/wp-fastest-cache/trunk@856214 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/css-utilities.php
+++ inc/css-utilities.php
@@ -62,7 +62,7 @@
 
 			$httpHost = str_replace("www.", "", $_SERVER["HTTP_HOST"]); 
 			if(preg_match("/href=[\"\'](.*?)[\"\']/", $link, $href)){
-				if(strpos($href[1], $httpHost)){
+				if(@strpos($href[1], $httpHost)){
 					if(strpos($href[1], $contentUrl."/themes") || strpos($href[1], $contentUrl."/plugins")){
 						return $href[1];
 					}
