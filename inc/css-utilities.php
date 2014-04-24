1fff83ed397a1491a37ece12e8e151c66500144c
emrevona <emrevona@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-24 20:21:28 +0000

    improvement of minift css
    git-svn-id: https://plugins.svn.wordpress.org/wp-fastest-cache/trunk@901965 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/css-utilities.php
+++ inc/css-utilities.php
@@ -168,86 +168,88 @@
 	     */
 	    protected function _process($css)
 	    {
-	        $css = str_replace("\r\n", "\n", $css);
+	        //$css = str_replace("\r\n", "\n", $css);
+
+	        $css = preg_replace("/^\s+/m", "", ((string) $css));
 	        
 	        // preserve empty comment after '>'
 	        // http://www.webdevout.net/css-hacks#in_css-selectors
-	        $css = preg_replace('@>/\\*\\s*\\*/@', '>/*keep*/', $css);
+	        // $css = preg_replace('@>/\\*\\s*\\*/@', '>/*keep*/', $css);
 	        
-	        // preserve empty comment between property and value
-	        // http://css-discuss.incutio.com/?page=BoxModelHack
-	        $css = preg_replace('@/\\*\\s*\\*/\\s*:@', '/*keep*/:', $css);
-	        $css = preg_replace('@:\\s*/\\*\\s*\\*/@', ':/*keep*/', $css);
+	        // // preserve empty comment between property and value
+	        // // http://css-discuss.incutio.com/?page=BoxModelHack
+	        // $css = preg_replace('@/\\*\\s*\\*/\\s*:@', '/*keep*/:', $css);
+	        // $css = preg_replace('@:\\s*/\\*\\s*\\*/@', ':/*keep*/', $css);
 	        
 	        // apply callback to all valid comments (and strip out surrounding ws
 	        $css = preg_replace_callback('@\\s*/\\*([\\s\\S]*?)\\*/\\s*@'
 	            ,array($this, '_commentCB'), $css);
 
-	        // remove ws around { } and last semicolon in declaration block
-	        $css = preg_replace('/\\s*{\\s*/', '{', $css);
-	        $css = preg_replace('/;?\\s*}\\s*/', '}', $css);
+	        // // remove ws around { } and last semicolon in declaration block
+	        // $css = preg_replace('/\\s*{\\s*/', '{', $css);
+	        // $css = preg_replace('/;?\\s*}\\s*/', '}', $css);
 	        
-	        // remove ws surrounding semicolons
-	        //causes break css down
-	        // $css = preg_replace('/\\s*;\\s*/', ';', $css);
+	        // // remove ws surrounding semicolons
+	        // //causes break css down
+	        // // $css = preg_replace('/\\s*;\\s*/', ';', $css);
 	        
-	        // remove ws around urls
-	        $css = preg_replace('/
-	                url\\(      # url(
-	                \\s*
-	                ([^\\)]+?)  # 1 = the URL (really just a bunch of non right parenthesis)
-	                \\s*
-	                \\)         # )
-	            /x', 'url($1)', $css);
+	        // // remove ws around urls
+	        // $css = preg_replace('/
+	        //         url\\(      # url(
+	        //         \\s*
+	        //         ([^\\)]+?)  # 1 = the URL (really just a bunch of non right parenthesis)
+	        //         \\s*
+	        //         \\)         # )
+	        //     /x', 'url($1)', $css);
 	        
-	        // remove ws between rules and colons
-	        $css = preg_replace('/
-	                \\s*
-	                ([{;])              # 1 = beginning of block or rule separator 
-	                \\s*
-	                ([\\*_]?[\\w\\-]+)  # 2 = property (and maybe IE filter)
-	                \\s*
-	                :
-	                \\s*
-	                (\\b|[#\'"])        # 3 = first character of a value
-	            /x', '$1$2:$3', $css);
+	        // // remove ws between rules and colons
+	        // $css = preg_replace('/
+	        //         \\s*
+	        //         ([{;])              # 1 = beginning of block or rule separator 
+	        //         \\s*
+	        //         ([\\*_]?[\\w\\-]+)  # 2 = property (and maybe IE filter)
+	        //         \\s*
+	        //         :
+	        //         \\s*
+	        //         (\\b|[#\'"])        # 3 = first character of a value
+	        //     /x', '$1$2:$3', $css);
 	        
-	        // remove ws in selectors
-	        $css = preg_replace_callback('/
-	                (?:              # non-capture
-	                    \\s*
-	                    [^~>+,\\s]+  # selector part
-	                    \\s*
-	                    [,>+~]       # combinators
-	                )+
-	                \\s*
-	                [^~>+,\\s]+      # selector part
-	                {                # open declaration block
-	            /x'
-	            ,array($this, '_selectorsCB'), $css);
+	        // // remove ws in selectors
+	        // $css = preg_replace_callback('/
+	        //         (?:              # non-capture
+	        //             \\s*
+	        //             [^~>+,\\s]+  # selector part
+	        //             \\s*
+	        //             [,>+~]       # combinators
+	        //         )+
+	        //         \\s*
+	        //         [^~>+,\\s]+      # selector part
+	        //         {                # open declaration block
+	        //     /x'
+	        //     ,array($this, '_selectorsCB'), $css);
 	        
-	        // minimize hex colors
-	        $css = preg_replace('/([^=])#([a-f\\d])\\2([a-f\\d])\\3([a-f\\d])\\4([\\s;\\}])/i'
-	            , '$1#$2$3$4$5', $css);
+	        // // minimize hex colors
+	        // $css = preg_replace('/([^=])#([a-f\\d])\\2([a-f\\d])\\3([a-f\\d])\\4([\\s;\\}])/i'
+	        //     , '$1#$2$3$4$5', $css);
 	        
-	        // remove spaces between font families
-	        $css = preg_replace_callback('/font-family:([^;}]+)([;}])/'
-	            ,array($this, '_fontFamilyCB'), $css);
+	        // // remove spaces between font families
+	        // $css = preg_replace_callback('/font-family:([^;}]+)([;}])/'
+	        //     ,array($this, '_fontFamilyCB'), $css);
 	        
-	        $css = preg_replace('/@import\\s+url/', '@import url', $css);
+	        // $css = preg_replace('/@import\\s+url/', '@import url', $css);
 	        
-	        // replace any ws involving newlines with a single newline
-	        $css = preg_replace('/[ \\t]*\\n+\\s*/', "\n", $css);
+	        // // replace any ws involving newlines with a single newline
+	        // $css = preg_replace('/[ \\t]*\\n+\\s*/', "\n", $css);
 	        
-	        // separate common descendent selectors w/ newlines (to limit line lengths)
-	        $css = preg_replace('/([\\w#\\.\\*]+)\\s+([\\w#\\.\\*]+){/', "$1\n$2{", $css);
+	        // // separate common descendent selectors w/ newlines (to limit line lengths)
+	        //$css = preg_replace('/([\\w#\\.\\*]+)\\s+([\\w#\\.\\*]+){/', "$1\n$2{", $css);
 	        
-	        // Use newline after 1st numeric value (to limit line lengths).
-	        $css = preg_replace('/
-	            ((?:padding|margin|border|outline):\\d+(?:px|em)?) # 1 = prop : 1st numeric value
-	            \\s+
-	            /x'
-	            ,"$1\n", $css);
+	        // // Use newline after 1st numeric value (to limit line lengths).
+	        // $css = preg_replace('/
+	        //     ((?:padding|margin|border|outline):\\d+(?:px|em)?) # 1 = prop : 1st numeric value
+	        //     \\s+
+	        //     /x'
+	        //     ,"$1\n", $css);
 	        
 	        // prevent triggering IE6 bug: http://www.crankygeek.com/ie6pebug/
 	        //$css = preg_replace('/:first-l(etter|ine)\\{/', ':first-l$1 {', $css);
