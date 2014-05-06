d30c4fe4a63842ba09a8686a5ec25577ca97e595
emrevona <emrevona@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-06 18:44:03 +0000

    minify css issue for data:application/x-font-woff
    git-svn-id: https://plugins.svn.wordpress.org/wp-fastest-cache/trunk@909168 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/css-utilities.php
+++ inc/css-utilities.php
@@ -113,7 +113,7 @@
 			$matches[1] = str_replace(array("\"","'"), "", $matches[1]);
 			if(!$matches[1]){
 				$matches[1] = "";
-			}else if(preg_match("/^(\/\/|http|\/\/fonts|data:image)/", $matches[1])){
+			}else if(preg_match("/^(\/\/|http|\/\/fonts|data:image|data:application)/", $matches[1])){
 				$matches[1] = $matches[1];
 			}else if(preg_match("/^\//", $matches[1])){
 				$homeUrl = str_replace(array("http:", "https:"), "", home_url());
