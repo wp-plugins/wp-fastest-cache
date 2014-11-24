e0410a6c2e065760495b89d3296ae7999e288a37
emrevona <emrevona@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-24 12:03:52 +0000

    mobile-cache.php
    git-svn-id: https://plugins.svn.wordpress.org/wp-fastest-cache/trunk@1031707 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpFastestCache.php
+++ wpFastestCache.php
@@ -50,8 +50,8 @@ GNU General Public License for more details.
 
 				register_deactivation_hook( __FILE__, array('WpFastestCache', 'deactivate') );
 
-				if(file_exists($this->getProLibraryPath("mobile-cache.php"))){
-					include_once $this->getProLibraryPath("mobile-cache.php");
+				if($this->isPluginActive("wp-fastest-cache-premium/wpFastestCachePremium.php")){
+					include_once $this->get_premium_path("mobile-cache.php");
 				}
 
 				if($this->isPluginActive("wp-fastest-cache-premium/wpFastestCachePremium.php")){
