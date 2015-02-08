8c98aea1fa73a3d17a8aea66e45cb8b506876714
emrevona <emrevona@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-08 21:32:16 +0000

    promotion sldie effect
    git-svn-id: https://plugins.svn.wordpress.org/wp-fastest-cache/trunk@1085157 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/admin.php
+++ inc/admin.php
@@ -1318,7 +1318,7 @@
 				</div>
 			</div>
 			<?php if(!class_exists("WpFastestCacheImageOptimisation")){ ?>
-				<div id="wpfc-promotion" style="bottom: -153px; position: fixed; z-index: 20; right: 20px;cursor: pointer;">
+				<div id="wpfc-promotion" style="height:250px; bottom: -153px; position: fixed; z-index: 9999; right: 20px;cursor: pointer;">
 					<img src="<?php echo plugins_url("wp-fastest-cache/images/promotions/promotion-1.jpg"); ?>">
 				</div>
 				<script type="text/javascript">
@@ -1326,15 +1326,11 @@
 					var promotion_bottom = jQuery("#wpfc-promotion").css("bottom");
 
 					jQuery("#" + promotion_id).mouseenter(function(){
-						jQuery("#" + promotion_id).animate({
-							bottom: "5px"
-						}, 300, function() {});
+						jQuery("#" + promotion_id).css({"bottom" : "5px", "z-index" : "9999"});
 					});
 
 					jQuery("#" + promotion_id).mouseleave(function(){
-						jQuery("#" + promotion_id).animate({
-							bottom: promotion_bottom
-						}, 300, function() {});
+						jQuery("#" + promotion_id).css({"bottom" : promotion_bottom, "z-index" : "9999"});
 					});
 
 					jQuery("#" + promotion_id).click(function(){
