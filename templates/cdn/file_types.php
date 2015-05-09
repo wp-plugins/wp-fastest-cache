f620adb783c183c2a99218e61c28865a83b32841
emrevona <emrevona@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-09 18:12:02 +0000

    mp4 has been added for cdn
    git-svn-id: https://plugins.svn.wordpress.org/wp-fastest-cache/trunk@1156994 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/cdn/file_types.php
+++ templates/cdn/file_types.php
@@ -1,6 +1,6 @@
 <div class="wpfc-checkbox-list">
 	<?php
-		$types = array("css", "js", "gif", "png", "jpg", "jpeg", "ttf", "otf", "woff", "less");
+		$types = array("css", "js", "gif", "png", "jpg", "jpeg", "ttf", "otf", "woff", "less", "mp4");
 
         foreach ($types as $key => $value) {
             ?>
