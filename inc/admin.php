028eba2e3e36df4b32e232c246374871a2ce3c3a
emrevona <emrevona@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-22 22:04:36 +0000

    to remove comments
    git-svn-id: https://plugins.svn.wordpress.org/wp-fastest-cache/trunk@1185793 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/admin.php
+++ inc/admin.php
@@ -1367,32 +1367,6 @@
 																			jQuery("#revert-loader-toolbar").hide();
 																		});
 																	}
-
-																	// var warning_box = jQuery("#wpfc-plugin-setup-warning");
-																	// var message = "";
-																	// wpfc.dictionary = typeof wpfc.dictionary != "undefined" ? wpfc.dictionary : [];
-																	
-																	// message += typeof wpfc.dictionary[data.error_message] != "undefined" ? wpfc.dictionary[data.error_message] : data.error_message;
-																	// message += ". ";
-																	// message += typeof wpfc.dictionary["You need to activate the premium plugin manually"] != "undefined" ? wpfc.dictionary["You need to activate the premium plugin manually"] : "You need to activate the premium plugin manually";
-																	// message += ". ";
-																	// message += typeof wpfc.dictionary["Please read the tutorial"] != "undefined" ? wpfc.dictionary["Please read the tutorial"] : "Please read the tutorial"; 
-																	// message += ". ";
-
-																	// warning_box.find(".fieldRow").text(message);
-																	// warning_box.find("#wpfc-read-tutorial").click(function(){
-																	// 	warning_box.hide();
-																	// 	var win = window.open("http://www.wpfastestcache.com/warnings/how-to-activate-premium-version-manually/", '_blank');
-																	// 	win.focus();
-																	// });
-
-																	// var windowHeight = 70;//(jQuery(window).height() - warning_box.height())/2;
-																	// var windowWidth = (jQuery(window).width() - warning_box.width())/2;
-
-																	// warning_box.css({"top": windowHeight, "left": windowWidth, "display" : "block", "position" : "fixed"});
-
-																	// jQuery("#revert-loader-toolbar").hide();
-
 																}
 																console.log(data, "data");
 															}
